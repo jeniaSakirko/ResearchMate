f0a63554ac38d9ee267958749c63291b25e98f1e
liorjanah <106554054+liorjanah@users.noreply.github.com>
2022-08-24 17:33:48 +0300

    Research page (#52)
    * add research.js and add const to the const.js
    
    * create drop list from DB
    
    * delete unnecessary const
    
    * add assign
    
    * add assign on change
    
    * add selectedResearch.id

---
+++ frontend/src/components/api/research.js
@@ -0,0 +1,41 @@
+import {RESEARCH_GET_ALL,RESEARCH_ASSIGH} from "../common/consts";
+import axios from "axios";
+
+
+export const getAll = async (token) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+        },
+    };
+
+    try {
+        const res = await axios
+            .get(RESEARCH_GET_ALL, config);
+        return res.data;
+    } catch (e) {
+        console.log(e);
+    } 
+};
+
+export const assign = async (token, participant_id,research_id) => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Token ${token}`,
+        },
+    };
+
+    const body = JSON.stringify({participant_id});
+    try {
+        const assign = RESEARCH_GET_ALL+"/"+research_id+RESEARCH_ASSIGH;
+        const res = await axios
+            .post(assign, body, config);
+        return res.data.token;
+    } catch (e) {
+        console.log(e);
+    }
+
+
+};
---
+++ frontend/src/components/pages/RegisterToResearch.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect, useRef } from 'react';
+import { Dropdown } from 'primereact/dropdown';
+import '../css/ChangeStatus.css';
+import {getAll,assign} from '../api/research';
+import {Button} from 'primereact/button';
+const token ="5d3a364c8096f0c6e167f763e58464793f008bdb3500044975dc3a98979a5e48";
+export const RegisterToResearch = () => {
+
+    useEffect(() => {
+        getAll(token).then(data => {
+            setResearchs(data);
+        });
+    }, []);
+
+
+    const [selectedResearch, setSelectedResearch] = useState(null);
+    const [researchs, setResearchs] = useState(null);
+    
+    const onResearchChange = (e) => {
+        setSelectedResearch(e.value);
+    }
+
+    const onAssign = async () => {
+        const vla = await assign(token,"9",selectedResearch.id)
+        console.log(vla)
+    }
+
+  
+    return (
+        <div  className="flex justify-content-center aligned-items-center vertical-align-middle">
+            <div className="card">
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 ">
+                <h5>Select a research</h5>
+                <Dropdown value={selectedResearch} options={researchs} onChange={onResearchChange} optionLabel="name" placeholder="Select a research" />
+                <Button onClick={onAssign} label="Register To Research" className="p-button-rounded"/>
+                </div> 
+            </div>       
+        </div>
+    );
+}
