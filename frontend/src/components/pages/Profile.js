96e7dc230b0763afc0231226e02e13d1ec69fa53
ggalfrin <gaby.galfrin@cyberark.com>
2022-09-02 19:17:56 +0300

    profile template + design

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -1,61 +1,212 @@
+import React, {useContext, useState} from 'react';
+import {InputText} from 'primereact/inputtext';
+import {Button} from 'primereact/button';
+import {Link} from 'react-router-dom';
+import {login} from "../api/auth";
+import {Navigate} from 'react-router-dom';
+//####
 import myData from "./customers-large-backend.json";
-import { InputText } from 'primereact/inputtext';
+// import { InputText } from 'primereact/inputtext';
+import '../css/OrderListDemo.css';
 
+// export const Login = () => {
 export const Profile = () => {
+    
     console.log (myData)
-
     const currentUser = myData[0];
     console.log (currentUser);
 
-    return (
-    <div style={{
-        borderStyle: "solid"
-    }}>
-            <br/>
-            <div>
-            <label>Username</label> &emsp;
-            <InputText value={currentUser.base_user.user.username} disabled={true} type="text" placeholder="Username" id="username" />
-            </div>
-            <br/>
-            <div>
-            <label>Email</label> &emsp;
-            <InputText value={currentUser.base_user.user.email} disabled={true} type="text" placeholder="Email" id="username" />
-            </div>
-            <br/>
-            <div>
-            <label>FirstName</label> &emsp;
-            <InputText value={currentUser.base_user.user.first_name} disabled={true} type="text" placeholder="FirstName" id="username" />
-            </div>
-            <br/>
-            <div>
-            <label>LastName</label> &emsp;
-            <InputText value={currentUser.base_user.user.last_name} disabled={true} type="text" placeholder="LastName" id="username" />
-            </div>
-            <br/>
-            <div>
-            <label>PhoneNumber</label> &emsp;
-            <InputText value={currentUser.base_user.phone_number} disabled={true} type="text" placeholder="PhoneNumber" id="username" />
-            </div>
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
 
 
-    </div>
+    const onLogin = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
 
+    const onSuspendUser = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
 
+    const onDisable = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
+
+    const onAssign = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
+
+    const onUnAssign = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
+
+    const onUpdateMeeting = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
+
+    const onComment = async () => {
+        const data = await login(username, password);
+        localStorage.setItem("userToken", data.token);
+    }
     
+    return (
 
-    )
+        <div className="flex justify-content-center aligned-items-center vertical-align-middle">
+            {/* TODO: uncomment! {userToken ? <Navigate to="/test/"/> : null}*/}
+            <div className="card" style={{borderColor: "red"}}>
+
+                {/* start of blue */}
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 " style={{borderColor: "blue"}}>
+                    <h3 className="align-items-center">Profile Information</h3>
+
+                    <span className="p-input-icon-left">
+                        <label>Username</label> &emsp;
+                    <InputText value={currentUser.base_user.user.username} disabled={true} type="text" placeholder="Username" id="username" />
+                    </span>
+
+                    <span className="p-input-icon-left">
+                    <label>Email</label> &emsp;
+                    <InputText value={currentUser.base_user.user.email} disabled={true} type="text" placeholder="Email" id="username" />
+                    </span>
+
+                    <span className="p-input-icon-left">
+                    <label>FirstName</label> &emsp;
+                    <InputText value={currentUser.base_user.user.first_name} disabled={true} type="text" placeholder="FirstName" id="username" />
+                    </span>
+
+                    <span className="p-input-icon-left">
+                    <label>LastName</label> &emsp;
+                    <InputText value={currentUser.base_user.user.last_name} disabled={true} type="text" placeholder="LastName" id="username" />
+                    </span>
+            
+                    <span className="p-input-icon-left">
+                    <label>PhoneNumber</label> &emsp;
+                    <InputText value={currentUser.base_user.phone_number} disabled={true} type="text" placeholder="PhoneNumber" id="username" />
+                    </span>
+
+                    <Button onClick={onSuspendUser} label="Suspend User" className="p-button-rounded"/>
+
+                    <Button onClick={onDisable} label="Disable" className="p-button-rounded"/> 
+                    <br></br>
+                </div>  {/* end of blue */}
+                                
+                {/* start of blue */}
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 " style={{borderColor: "blue"}}>
+                                    <h3 className="align-items-center">Past Researches</h3>
+
+                                    <span className="p-input-icon-left">
+                                        <label>*list of researches.....*</label> &emsp;
+                                    </span>
+
+                                    <br></br>
+                                </div>  {/* end of blue */}
+
+                {/* start of blue */}
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 " style={{borderColor: "blue"}}>
+                                    <h3 className="align-items-center">Assign Research</h3>
+
+                                    <span className="p-input-icon-left">
+                                        <label>*list of researches.....*</label> &emsp;
+                                    </span>
+                                    <Button onClick={onAssign} label="Assign" className="p-button-rounded"/>
+
+                                    <Button onClick={onUnAssign} label="UnAssign" className="p-button-rounded"/> 
+                                    <br></br>
+                                </div>  {/* end of blue */}
+
+                {/* start of blue */}
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 " style={{borderColor: "blue"}}>
+                                    <h3 className="align-items-center">Update A Meeting</h3>
+
+                                    <Button onClick={onUpdateMeeting} label="Update Meeting" className="p-button-rounded"/>
+
+                                    <br></br>
+                                </div>  {/* end of blue */}
+
+                {/* start of blue */}
+                <div className="flex flex-column align-items-center justify-content-center card-container gap-3
+                                surface-overlay border-round border-1 shadow-1 p-5 py-0 m-3 " style={{borderColor: "blue"}}>
+                                    <h3 className="align-items-center">Add A Comment</h3>
+
+                                    <Button onClick={onComment} label="Add A Comment" className="p-button-rounded"/>
+
+                                    <br></br>
+                                </div>  {/* end of blue */}
 
 
-
+            </div> {/* end of red */}
+        </div>
+    );
 }
 
 
 
 
+// import myData from "./customers-large-backend.json";
+// import { InputText } from 'primereact/inputtext';
+// import '../css/OrderListDemo.css';
+
+// export const Profile = () => {
+//     console.log (myData)
+
+//     const currentUser = myData[0];
+//     console.log (currentUser);
+
+//     return (
+//     <div style={{borderStyle: "solid"}}>
+//             <br/>
+//             <div>
+//             <label>Username</label> &emsp;
+//             <InputText value={currentUser.base_user.user.username} disabled={true} type="text" placeholder="Username" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>Email</label> &emsp;
+//             <InputText value={currentUser.base_user.user.email} disabled={true} type="text" placeholder="Email" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>FirstName</label> &emsp;
+//             <InputText value={currentUser.base_user.user.first_name} disabled={true} type="text" placeholder="FirstName" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>LastName</label> &emsp;
+//             <InputText value={currentUser.base_user.user.last_name} disabled={true} type="text" placeholder="LastName" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>PhoneNumber</label> &emsp;
+//             <InputText value={currentUser.base_user.phone_number} disabled={true} type="text" placeholder="PhoneNumber" id="username" />
+//             </div>
+
+
+//     </div>
+
+//     )
+
+// }
 
 
 
 
+
+
+
+// ##########################################################################
+// ##########################################################################
+// ##########################################################################
+// ##########################################################################
 
 // // import React from 'react';
 // // import { OrderList } from 'primereact/orderlist';
@@ -65,13 +216,19 @@ export const Profile = () => {
 // import React, { useState, useEffect } from 'react';
 // import { OrderList } from 'primereact/orderlist';
 // import { ProductService } from './ProductService';
-// import './OrderListDemo.css';
+// import '../css/OrderListDemo.css';
+// import myData from "./customers-large-backend.json";
+// import { InputText } from 'primereact/inputtext';
 
 // export const Profile = () => {
 
 // // export const OrderListDemo = () => {
 //         const [products, setProducts] = useState([]);
 //         const productService = new ProductService();
+//         console.log (myData)
+
+//         const currentUser = myData[0];
+//         console.log (currentUser);
 
 //         useEffect(() => {
 //             productService.getProductsSmall().then(data => setProducts(data));
@@ -105,13 +262,45 @@ export const Profile = () => {
 
 //             <div className="orderlist-demo">
 //                 <div><p>111111111111</p></div>
-//                 <div className="card">
-//                     <OrderList value={products} header="List of Products" dragdrop listStyle={{height:'auto'}} dataKey="id"
+//                 {/* <div className="card"> */}
+//                     {/* <OrderList value={products} header="List of Products" dragdrop listStyle={{height:'auto'}} dataKey="id"
 //                         itemTemplate={itemTemplate} onChange={(e) => setProducts(e.value)}>
 //                             <div><p>2222222222</p></div>
-//                         </OrderList>
+//                         </OrderList> */}
+
+// <div style={{borderStyle: "solid"}}>
+//             <br/>
+//             <div>
+//             <label>Username</label> &emsp;
+//             <InputText value={currentUser.base_user.user.username} disabled={true} type="text" placeholder="Username" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>Email</label> &emsp;
+//             <InputText value={currentUser.base_user.user.email} disabled={true} type="text" placeholder="Email" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>FirstName</label> &emsp;
+//             <InputText value={currentUser.base_user.user.first_name} disabled={true} type="text" placeholder="FirstName" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>LastName</label> &emsp;
+//             <InputText value={currentUser.base_user.user.last_name} disabled={true} type="text" placeholder="LastName" id="username" />
+//             </div>
+//             <br/>
+//             <div>
+//             <label>PhoneNumber</label> &emsp;
+//             <InputText value={currentUser.base_user.phone_number} disabled={true} type="text" placeholder="PhoneNumber" id="username" />
+//             </div>
+//     </div>
+
+
+
+
 //                         <div><p>3333333333333</p></div>
-//                 </div>
+//                 {/* </div> */}
 //                 <div><p>4444444444</p></div>
 //             </div>
 //         );
