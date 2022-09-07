a30fa7081362bf56d78428be15535145227cfbdc
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-09-07 17:00:42 +0300

    Add script that will start prod
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

---
+++ startServer.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+echo "Start reloading the server, may the gods be with us"
+echo 
+echo "--------------------------"
+echo 
+echo "Running docker compose down"
+sudo docker-compose down
+
+echo "Undo all local changes"
+git checkout -- .
+echo 
+echo "--------------------------"
+echo 
+echo "Getting the latest changes"
+git pull --ff-only
+echo 
+echo "--------------------------"
+echo 
+echo "Change the port"
+sed -i 's/8000:8000/8080:8000/g' docker-compose.yml
+sed -i 's/3000:3000/443:3000/g' docker-compose.yml
+echo 
+echo "--------------------------"
+echo 
+echo "Change server address"
+sed -i "s,http://localhost:8000,http://vmedu259.mtacloud.co.il:8080,g" frontend/src/index.js
+echo 
+echo "--------------------------"
+echo 
+echo "The money time! lets start the server"
+sudo docker-compose up -d
+
