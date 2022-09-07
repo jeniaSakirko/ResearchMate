82c2134d1ef936c22cccccaab33d9a1925ae429a
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-09-07 21:39:21 +0300

    Add assign part of profile
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- frontend/src/components/common/consts.js
+++ frontend/src/components/common/consts.js
@@ -12,6 +12,7 @@ export const PARTICIPANT_REGISTER_URL = BASE_PARTICIPANT_API + '/register';
 export const PARTICIPANT_GET_ALL_URL = BASE_PARTICIPANT_API;
 export const PARTICIPANT_GET_ALL_FORMS_URL = BASE_PARTICIPANT_API + '/forms';
 export const PARTICIPANT_PUT_AGREE_ON_FORM_URL = PARTICIPANT_GET_ALL_FORMS_URL + '/agree';
+export const PARTICIPANT_GET_RESEARCH = '/research';
 
 //Research
 export const  RESEARCH_GET_ALL = '/api/research';
--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -3,18 +3,28 @@ import {InputText} from 'primereact/inputtext';
 import {Button} from 'primereact/button';
 import {useParams} from "react-router-dom";
 import {Splitter, SplitterPanel} from 'primereact/splitter';
+import {Dropdown} from "primereact/dropdown";
 
-import {getParticipant} from "../api/participant";
+import {getParticipant, getParticipantResearchHistory} from "../api/participant";
+import {assign, getAll, unassign} from "../api/research";
 
 export const Profile = () => {
     const {participantId} = useParams()
     const [currentUser, setCurrentUser] = useState('');
+    const [selectedResearch, setSelectedResearch] = useState(null);
+    const [availableResearches, setAvailableResearches] = useState(null);
+    const [userResearches, setUserResearches] = useState(null);
 
     useEffect(() => {
         getParticipant(participantId).then(data => {
             setCurrentUser(data);
-            console.log(data);
         });
+        getAll().then(data => {
+            setAvailableResearches(data);
+        });
+        getParticipantResearchHistory(participantId, "all").then(data => {
+            setUserResearches(data);
+        })
     }, [])
 
     const onSuspendUser = async () => {
@@ -25,20 +35,24 @@ export const Profile = () => {
         console.log("onDisable");
     }
 
-    const onAssign = async () => {
-        console.log("onAssign");
-    }
-
-    const onUnAssign = async () => {
-        console.log("onUnAssign");
-    }
-
     const onUpdateMeeting = async () => {
         console.log("onUpdateMeeting");
     }
 
     const onComment = async () => {
         console.log("onComment");
+    }
+
+    const onResearchChange = (e) => {
+        setSelectedResearch(e.value);
+    }
+
+    const onAssign = async () => {
+        await assign(participantId, selectedResearch.id)
+    }
+
+    const onUnAssign = async () => {
+        await unassign(participantId, selectedResearch.id)
     }
 
     return (
@@ -74,7 +88,7 @@ export const Profile = () => {
                          <label>Phone Number</label> &emsp;
                         <InputText value={currentUser.base_user?.phone_number} disabled={true} type="text"
                                    placeholder="Phone Number" id="phone"/>
-                        </span>
+                    </span>
                     <div>
                         <Button onClick={onSuspendUser} label="Suspend User" className="p-button-rounded"/>
                         <Button onClick={onDisable} label="Disable User" className="p-button-rounded"/>
@@ -92,16 +106,19 @@ export const Profile = () => {
                         </SplitterPanel>
 
                         <SplitterPanel className="flex align-items-center justify-content-center" size={20} minSize={5}>
-                            <div className="flex flex-column align-items-center justify-content-center gap-3">
-                                <h3 className="align-items-center">Assign Research</h3>
-                                <span className="p-input-icon-left">
-                                    <label>*list of researches.....*</label> &emsp;
-                                </span>
-                                <div className="p-2">
-                                    <Button onClick={onAssign} label="Assign" className="p-button-rounded"/>
-                                    <Button onClick={onUnAssign} label="UnAssign" className="p-button-rounded"/>
+                            <div className="flex flex-column align-items-center justify-content-center gap-3 m-3">
+                                <h5>Select a research</h5>
+                                <Dropdown value={selectedResearch} options={availableResearches}
+                                          onChange={onResearchChange}
+                                          optionLabel="name" placeholder="Select a research"/>
+                                <div className="gap-2">
+                                    <Button onClick={onAssign} label="Register To Research"
+                                            className="p-button-rounded"/>
+                                    <Button onClick={onUnAssign} label="Cancel Register To Research"
+                                            className="p-button-rounded"/>
                                 </div>
                             </div>
+
                         </SplitterPanel>
 
                         <SplitterPanel className="flex align-items-center justify-content-center" size={20} minSize={5}>
