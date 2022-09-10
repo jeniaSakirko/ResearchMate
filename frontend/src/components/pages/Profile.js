48ebfe26c3913804cd22119c85eb3a7589874410
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-09-10 12:26:22 +0300

    Add profile info - Profile is saved as table and have hidden edit btn - Past research is table and show all drop / done
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- frontend/src/components/pages/Profile.js
+++ frontend/src/components/pages/Profile.js
@@ -4,28 +4,76 @@ import {Button} from 'primereact/button';
 import {useParams} from "react-router-dom";
 import {Splitter, SplitterPanel} from 'primereact/splitter';
 import {Dropdown} from "primereact/dropdown";
+import {DataTable} from 'primereact/datatable';
+import {Column} from 'primereact/column';
 
 import {getParticipant, getParticipantResearchHistory} from "../api/participant";
 import {assign, getAll, unassign} from "../api/research";
+import {getUserType} from "../common/UserContext";
+
+import "../css/DataTable.css"
 
 export const Profile = () => {
     const {participantId} = useParams()
-    const [currentUser, setCurrentUser] = useState('');
+    const [userInfoTbl, setUserInfoTbl] = useState([]);
     const [selectedResearch, setSelectedResearch] = useState(null);
     const [availableResearches, setAvailableResearches] = useState(null);
-    const [userResearches, setUserResearches] = useState(null);
+    const [userProfileLoading, setUserProfileLoading] = useState(true);
+    const [editingRows, setEditingRows] = useState({});
+    const [hideEditBtn, setHideEditBtn] = useState(true);
+    const [pastResearch, setPastResearch] = useState([]);
+
 
     useEffect(() => {
         getParticipant(participantId).then(data => {
-            setCurrentUser(data);
+            generateUserInfoTable(data).then(userInfo => {
+                getParticipantResearchHistory(participantId, "all").then(data => {
+                    getUserType().then(res => {
+                        if (res.toLowerCase() === "participant") {
+                            setHideEditBtn(false);
+                        }
+                        setUserProfileLoading(false);
+                    });
+                    updateUserInfoTableResearch(userInfo, data);
+                    updateUserPastResearches(data);
+                });
+            });
         });
         getAll().then(data => {
             setAvailableResearches(data);
         });
-        getParticipantResearchHistory(participantId, "all").then(data => {
-            setUserResearches(data);
-        })
+
     }, [])
+
+    const generateUserInfoTable = async (data) => {
+        let userInfo = []
+        userInfo.push({id: userInfo.length + 1, key: "Username", value: data.base_user?.user.username});
+        userInfo.push({id: userInfo.length + 1, key: "Emil", value: data.base_user?.user.email});
+        userInfo.push({id: userInfo.length + 1, key: "First Name", value: data.base_user?.user.first_name});
+        userInfo.push({id: userInfo.length + 1, key: "Last Name", value: data.base_user?.user.last_name});
+        userInfo.push({id: userInfo.length + 1, key: "Phone", value: data.base_user?.phone_number});
+        return userInfo;
+    }
+
+    const updateUserInfoTableResearch = async (userInfo, data) => {
+        for (const res of data) {
+            if (res.status.toLocaleString() === "Assigned" || res.status.toLocaleString() === "In Progress") {
+                userInfo.push({id: userInfo.length + 1, key: "Research", value: res.name + " (" + res.status + ")"})
+            }
+        }
+        setUserInfoTbl(userInfo);
+    }
+
+    const updateUserPastResearches = async (data) => {
+        let pastInfo = []
+        for (const res of data) {
+            if (res.status.toLocaleString() === "Done" || res.status.toLocaleString() === "Drop") {
+                pastInfo.push({id: pastInfo.length + 1, name: res.name, status: res.status})
+            }
+        }
+        setPastResearch(pastInfo);
+    }
+
 
     const onSuspendUser = async () => {
         console.log("onSuspendUser");
@@ -55,40 +103,43 @@ export const Profile = () => {
         await unassign(participantId, selectedResearch.id)
     }
 
+    const onRowEditComplete = (e) => {
+        let _userInfoTbl = [...userInfoTbl];
+        let {newData, index} = e;
+
+        _userInfoTbl[index] = newData;
+
+        setUserInfoTbl(_userInfoTbl);
+    }
+
+    const onRowEditChange = (e) => {
+        setEditingRows(e.data);
+    }
+
+    const textEditor = (options) => {
+        return <InputText type="text" value={options.value} style={{width: "100%"}}
+                          onChange={(e) => options.editorCallback(e.target.value)}/>;
+    }
+
     return (
         <div className="card">
             <Splitter style={{height: '740px'}}>
                 <SplitterPanel className="flex flex-column align-items-center justify-content-center gap-3" size={80}
                                minSize={20}>
-                    <h3 className="align-items-center">Profile Information</h3>
-                    <span className="p-input-icon-left">
-                        <label>Username</label> &emsp;
-                        <InputText value={currentUser.base_user?.user.username} disabled={true} type="text"
-                                   placeholder="Username" id="username"/>
-                        </span>
-
-                    <span className="p-input-icon-left">
-                         <label>Email</label> &emsp;
-                        <InputText value={currentUser.base_user?.user.email} disabled={true} type="text"
-                                   placeholder="Email" id="email"/>
-                        </span>
-
-                    <span className="p-input-icon-left">
-                         <label>First Name</label> &emsp;
-                        <InputText value={currentUser.base_user?.user.first_name} disabled={true} type="text"
-                                   placeholder="First Name" id="fName"/>
-                        </span>
-
-                    <span className="p-input-icon-left">
-                         <label>Last Name</label> &emsp;
-                        <InputText value={currentUser.base_user?.user.last_name} disabled={true} type="text"
-                                   placeholder="Last Name" id="lName"/>
-                        </span>
-                    <span className="p-input-icon-left">
-                         <label>Phone Number</label> &emsp;
-                        <InputText value={currentUser.base_user?.phone_number} disabled={true} type="text"
-                                   placeholder="Phone Number" id="phone"/>
-                    </span>
+                    <h3 className="align-items-center">Profile Info</h3>
+                    <DataTable className="p-datatable-user-profile noHeader" value={userInfoTbl}
+                               style={{width: "100%"}}
+                               editMode="row" dataKey="id"
+                               loading={userProfileLoading} emptyMessage="No user info found."
+                               size="large" responsiveLayout="scroll" editingRows={editingRows}
+                               onRowEditChange={onRowEditChange}
+                               onRowEditComplete={onRowEditComplete}>
+                        <Column field="key" header="key" style={{border: "none"}}/>
+                        <Column field="value" header="value" editor={(options) => textEditor(options)}
+                                style={{border: "none"}}/>
+                        <Column rowEditor headerStyle={{width: '10%', minWidth: '8rem'}}
+                                bodyStyle={{textAlign: 'center'}} hidden={hideEditBtn} style={{border: "none"}}/>
+                    </DataTable>
                     <div>
                         <Button onClick={onSuspendUser} label="Suspend User" className="p-button-rounded"/>
                         <Button onClick={onDisable} label="Disable User" className="p-button-rounded"/>
@@ -97,11 +148,15 @@ export const Profile = () => {
                 <SplitterPanel size={80}>
                     <Splitter layout="vertical">
                         <SplitterPanel className="flex align-items-center justify-content-center" size={20} minSize={5}>
-                            <div className="flex flex-column align-items-center justify-content-center gap-3">
+                            <div className="flex flex-column align-items-center justify-content-center gap-3"
+                                 style={{width: '100%'}}>
                                 <h3 className="align-items-center">Past Researches</h3>
-                                <span className="p-input-icon-left">
-                                    <label>*list of past researches.....*</label> &emsp;
-                                </span>
+                                <DataTable value={pastResearch} scrollable scrollHeight="400px"
+                                           loading={userProfileLoading} style={{width: "100%"}}
+                                           emptyMessage="No past research were taken.">
+                                    <Column field="name" header="Name" style={{minWidth: '200px'}}/>
+                                    <Column field="status" header="Status" style={{minWidth: '200px'}}/>
+                                </DataTable>
                             </div>
                         </SplitterPanel>
 
