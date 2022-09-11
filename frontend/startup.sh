62a46b81745bd017187d57e90c4431c2e8c87fb2
Jenia Sakirko <Jenia.Sakirko@cyberark.com>
2022-09-11 10:14:05 +0300

    Add config to run front in prod
    Signed-off-by: Jenia Sakirko <Jenia.Sakirko@cyberark.com>

--- frontend/startup.sh
+++ frontend/startup.sh
@@ -1,4 +1,13 @@
 #!/bin/bash -ex
 
-npm install && npm audit --production
-(npm start > runserver.log 2>&1)
+env="local"
+
+if [ "$env" == "local" ]; then
+    npm install && npm audit --production
+    (npm start > runserver.log 2>&1)
+else
+    npm run build
+    npm install -g serve
+
+    (serve -s build > runserver.log 2>&1)
+fi
